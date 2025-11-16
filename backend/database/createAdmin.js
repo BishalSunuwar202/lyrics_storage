e5881f62fc03caf20040c85674547c7f05ad0a96
BishalSunuwar202 <bishalsunuwar24@gmail.com>
2025-11-16 17:29:20 +0545

    admin table

---
+++ backend/database/createAdmin.js
@@ -0,0 +1,23 @@
+import pool from "../db.js";
+import bcrypt from "bcryptjs";
+
+const createAdmin = async () => {
+  const adminQueryText =  `CREATE TABLE IF NOT EXISTS admin (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+)`
+    try {
+        const plain = "spaceX202@";
+const hash = await bcrypt.hash(plain, 10);
+console.log(hash);
+        await pool.query(adminQueryText)
+        console.log("admin table created if not existed")
+    } catch(err) {
+        console.log("error while creating admin table", err)
+        throw err
+    }
+
+}
+export default createAdmin;
